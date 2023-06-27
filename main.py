c28b906e71ba5c2d0e5fc1789c87803f7e6350c3
EMMANUEL-1985 <112596295+Emmanuel-1985@users.noreply.github.com>
2023-06-27 20:30:25 -0300

    Agregando algunas funciones
    funciones

--- main.py
+++ main.py
@@ -37,5 +37,107 @@ def mostrar_menu():
     print(Fore.CYAN+"7. Buscar contacto por nombre"+ Style.RESET_ALL)
     print(Fore.CYAN+"8. Mostrar lista de contactos"+ Style.RESET_ALL)
     print(Fore.CYAN+"9. Salir"+ Style.RESET_ALL)
+// Emmanuel Sbona
+def agregar_contacto2(Contacto2):
+    cursor.execute('''
+        INSERT INTO contactop (nombre, telefono, empresa, cargo,edad,correo,  whatsapp, facebook, instagram)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+        RETURNING id
+    ''', [Contacto2.getnombre, Contacto2.gettelefono, Contacto2.getempresa, Contacto2.getcargo, Contacto2.getedad,
+          Contacto2.getcorreo, Contacto2.getwhatsapp, Contacto2.getfacebook, Contacto2.getwhatsapp])
+    ContactoProfesional.id = cursor.fetchone()[0]
+    conexion.commit()
+    print("Contacto agregado exitosamente.")
 
+
+# Función para eliminar un contacto de la base de datos
+def eliminar_contacto():
+    try:
+        tabla = None
+        while tabla not in ["1", "2"]:
+            tabla = input("Ingrese el número de tabla en la que desea eliminar el contacto:\n1. ContactoPersonal\n2. ContactoProfesional\n")
+            if tabla not in ["1", "2"]:
+                print("Opción no válida. Por favor, ingrese 1 o 2.")
+        tabla = int(tabla)
+        contacto_id = None
+        while not isinstance(contacto_id, int):
+            try:
+                contacto_id = int(input("Ingrese el ID del contacto a eliminar: "))
+            except ValueError:
+                print("ID no válido. Por favor, ingrese un número entero.")
+        with conexion:
+            with conexion.cursor() as cursor:
+                if tabla == 1:
+                    cursor.execute('DELETE FROM contactos WHERE id = %s', (contacto_id,))
+                    print("Contacto eliminado exitosamente de la tabla contactos.")
+                elif tabla == 2:
+                    cursor.execute('DELETE FROM contactop WHERE id = %s', (contacto_id,))
+                    print("Contacto eliminado exitosamente de la tabla contactop.")
+                else:
+                    print("Tabla no válida. No se pudo eliminar el contacto.")
+                conexion.commit()
+    except Exception as e:
+        print(f'Ocurrió un error: {e}')
+    finally:
+        if cursor is not None:
+            cursor.close()
+
+
+# Función para modificar un contacto en la base de datos
+def modificar_contacto(contacto_id, nombre, telefono, empresa, cargo, whatsapp, facebook, instagram):
+    cursor.execute('''
+        UPDATE contactos
+        SET nombre = %s, telefono = %s, empresa = %s, cargo = %s,  whatsapp = %s, facebook = %s, instagram = %s
+        WHERE id = %s
+    ''', (nombre, telefono, empresa, cargo,  whatsapp, facebook, instagram, contacto_id))
+    conexion.commit()
+    print("Contacto modificado exitosamente.")
+
+
+# Función para agregar un evento a la base de datos
+def agregar_evento(fecha, descripcion, contacto_id):
+    cursor.execute('''
+        INSERT INTO eventos (fecha, descripcion, contacto_id)
+        VALUES (%s, %s, %s)
+    ''', (fecha, descripcion, contacto_id))
+    conexion.commit()
+    print("Evento agregado exitosamente.")
+
+
+# Función para visualizar eventos en una fecha determinada
+def visualizar_eventos(fecha):
+    cursor.execute('''
+        SELECT contactos.nombre, eventos.descripcion
+        FROM eventos
+        INNER JOIN contactos ON eventos.contacto_id = contactos.id
+        WHERE eventos.fecha = %s
+    ''', (fecha,))
+    eventos = cursor.fetchall()
+
+    if len(eventos) > 0:
+        print("Eventos para la fecha", fecha)
+        for evento in eventos:
+            print("Contacto:", evento[0])
+            print("Descripción:", evento[1])
+            print("---")
+    else:
+        print("No hay eventos para la fecha", fecha)
+
+#funcion para borrar evento
+def eliminar_evento_por_fecha(fecha):
+    cursor.execute(" DELETE FROM eventos WHERE fecha = %s", (fecha,))
+    conexion.commit()
+    print("Evento eliminado exitosamente.")
+
+
+# Función para buscar contactos por nombre
+def buscar_contacto(nombre):
+    cursor.execute("SELECT * FROM contactos WHERE nombre ILIKE %s", ('%' + nombre + '%',))
+    resultados = cursor.fetchall()
+    if len(resultados) > 0:
+        print("Resultados de búsqueda:")
+        for resultado in resultados:
+            print(resultado)
+    else:
+        print(Fore.GREEN+"No se encontraron contactos con ese nombre."+Style.RESET_ALL)
 
