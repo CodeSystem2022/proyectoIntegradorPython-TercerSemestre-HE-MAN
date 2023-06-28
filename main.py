7a652245b05cb4202c44e19c48c5a90dbf827e60
EMMANUEL-1985 <112596295+Emmanuel-1985@users.noreply.github.com>
2023-06-27 21:13:36 -0300

    Modificando algunas de las funciones

--- main.py
+++ main.py
@@ -38,108 +38,105 @@ def mostrar_menu():
     print(Fore.CYAN+"8. Mostrar lista de contactos"+ Style.RESET_ALL)
     print(Fore.CYAN+"9. Salir"+ Style.RESET_ALL)
 # Emmanuel Sbona
+def leer_opcion():
+    while True:
+        try:
+            opcion = int(input(Fore.MAGENTA+ Back.BLACK +"Ingrese una opción: "+ Style.RESET_ALL))
+            print("======================================")
+            if opcion < 1 or opcion > 9:
+                raise ValueError
+            return opcion
+        except ValueError:
+            print("Opción inválida. Por favor, ingrese un número del 1 al 9.")
+
+# Función para leer una opción del segundo menú
+def leer_opcion2():
+    while True:
+        try:
+            OPC = int(input(Fore.MAGENTA+ Back.BLACK +"Ingrese una opción: "+ Style.RESET_ALL))
+            print("======================================")
+            if OPC < 1 or OPC > 4:
+                raise ValueError
+            return OPC
+        except ValueError:
+            print("Opción inválida. Por favor, ingrese un número del 1 al 4.")
+
+
+# Función para agregar un contacto a la base de datos
+#Punto1
+def agregar_contacto(contacto):
+    cursor.execute('''
+        INSERT INTO contactos (nombre, telefono, empresa, cargo)
+        VALUES (%s, %s, %s, %s)
+        RETURNING id
+    ''', (contacto.getnombre, contacto.gettelefono, contacto.getempresa, contacto.getcargo))
+    contactoPersonal.id = cursor.fetchone()[0]
+    conexion.commit()
+    print("Contacto agregado exitosamente.")
+#Punto1
 def agregar_contacto2(Contacto2):
     cursor.execute('''
         INSERT INTO contactop (nombre, telefono, empresa, cargo,edad,correo,  whatsapp, facebook, instagram)
         VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
         RETURNING id
     ''', [Contacto2.getnombre, Contacto2.gettelefono, Contacto2.getempresa, Contacto2.getcargo, Contacto2.getedad,
-          Contacto2.getcorreo, Contacto2.getwhatsapp, Contacto2.getfacebook, Contacto2.getwhatsapp])
+          Contacto2.getcorreo, Contacto2.getwhatsapp, Contacto2.getfacebook, Contacto2.getinstagram])
     ContactoProfesional.id = cursor.fetchone()[0]
     conexion.commit()
     print("Contacto agregado exitosamente.")
+#Punto 1 EL submenu
+#Funcion para que al crear un contacto SE PUEDAD ELEJIR ENTRE CPROFESIONAL O PERSONAL
+def datos_menu_dos():
+    while True:
+        menu2()
+        OPC = leer_opcion2()
+
+        if OPC == 1:
+            nombre, telefono, empresa, cargo, edad, correo, whatsapp, facebook, instagram = pedir_Datos_Contacto_Profesional()
+            contacto_profesional = ContactoProfesional(id,nombre, telefono, empresa, cargo, edad, correo, whatsapp,facebook, instagram)
+            agregar_contacto2(contacto_profesional)
+        elif OPC == 2:
+             nombre, telefono, empresa, cargo = capturar_datos_contacto()
+             Contacto_personal = ContactoPersonal(id,nombre, telefono, empresa, cargo )
+             agregar_contacto(Contacto_personal)
+
+        elif OPC == 3:
+            ejecutar_agenda()
+        elif OPC == 4:
+            print("HA SALIDO DEL PROGRAMA!")
+            print("HASTA LA PROXIMA AMIGO!")
+            break
+        else:
+            print(Fore.RED+"LA OPCION QUE INGRESO ES INCORRECTA "+Style.RESET_ALL)
+            print(Fore.RED+"Igrese nuevamente una opcion "+Style.RESET_ALL)
+#Funcion para el segundo menu
+def menu2 ():
+    print(Fore.MAGENTA + Back.BLACK + "=====MENU_CONTACTO=====" + Style.RESET_ALL)
+    print(Fore.YELLOW + "1. Contacto Profesional" + Style.RESET_ALL)
+    print(Fore.YELLOW + "2. Contacto Personal" + Style.RESET_ALL)
+    print(Fore.YELLOW + "3. Volver al Menu Principal" + Style.RESET_ALL)
+    print(Fore.YELLOW + "4. Salir" + Style.RESET_ALL)
 
 
-# Función para eliminar un contacto de la base de datos
-def eliminar_contacto():
-    try:
-        tabla = None
-        while tabla not in ["1", "2"]:
-            tabla = input("Ingrese el número de tabla en la que desea eliminar el contacto:\n1. ContactoPersonal\n2. ContactoProfesional\n")
-            if tabla not in ["1", "2"]:
-                print("Opción no válida. Por favor, ingrese 1 o 2.")
-        tabla = int(tabla)
-        contacto_id = None
-        while not isinstance(contacto_id, int):
-            try:
-                contacto_id = int(input("Ingrese el ID del contacto a eliminar: "))
-            except ValueError:
-                print("ID no válido. Por favor, ingrese un número entero.")
-        with conexion:
-            with conexion.cursor() as cursor:
-                if tabla == 1:
-                    cursor.execute('DELETE FROM contactos WHERE id = %s', (contacto_id,))
-                    print("Contacto eliminado exitosamente de la tabla contactos.")
-                elif tabla == 2:
-                    cursor.execute('DELETE FROM contactop WHERE id = %s', (contacto_id,))
-                    print("Contacto eliminado exitosamente de la tabla contactop.")
-                else:
-                    print("Tabla no válida. No se pudo eliminar el contacto.")
-                conexion.commit()
-    except Exception as e:
-        print(f'Ocurrió un error: {e}')
-    finally:
-        if cursor is not None:
-            cursor.close()
-
-
-# Función para modificar un contacto en la base de datos
-def modificar_contacto(contacto_id, nombre, telefono, empresa, cargo, whatsapp, facebook, instagram):
-    cursor.execute('''
-        UPDATE contactos
-        SET nombre = %s, telefono = %s, empresa = %s, cargo = %s,  whatsapp = %s, facebook = %s, instagram = %s
-        WHERE id = %s
-    ''', (nombre, telefono, empresa, cargo,  whatsapp, facebook, instagram, contacto_id))
-    conexion.commit()
-    print("Contacto modificado exitosamente.")
-
-
-# Función para agregar un evento a la base de datos
-def agregar_evento(fecha, descripcion, contacto_id):
-    cursor.execute('''
-        INSERT INTO eventos (fecha, descripcion, contacto_id)
-        VALUES (%s, %s, %s)
-    ''', (fecha, descripcion, contacto_id))
-    conexion.commit()
-    print("Evento agregado exitosamente.")
-
-
-# Función para visualizar eventos en una fecha determinada
-def visualizar_eventos(fecha):
-    cursor.execute('''
-        SELECT contactos.nombre, eventos.descripcion
-        FROM eventos
-        INNER JOIN contactos ON eventos.contacto_id = contactos.id
-        WHERE eventos.fecha = %s
-    ''', (fecha,))
-    eventos = cursor.fetchall()
-
-    if len(eventos) > 0:
-        print("Eventos para la fecha", fecha)
-        for evento in eventos:
-            print("Contacto:", evento[0])
-            print("Descripción:", evento[1])
-            print("---")
-    else:
-        print("No hay eventos para la fecha", fecha)
-
-#funcion para borrar evento
-def eliminar_evento_por_fecha(fecha):
-    cursor.execute(" DELETE FROM eventos WHERE fecha = %s", (fecha,))
-    conexion.commit()
-    print("Evento eliminado exitosamente.")
-
-
-# Función para buscar contactos por nombre
-def buscar_contacto(nombre):
-    cursor.execute("SELECT * FROM contactos WHERE nombre ILIKE %s", ('%' + nombre + '%',))
-    resultados = cursor.fetchall()
-    if len(resultados) > 0:
-        print("Resultados de búsqueda:")
-        for resultado in resultados:
-            print(resultado)
-    else:
-        print(Fore.GREEN+"No se encontraron contactos con ese nombre."+Style.RESET_ALL)
+# Función para capturar los datos de un contacto desde el usuario
+def capturar_datos_contacto():
+    nombre = input("Ingrese el nombre: ")
+    telefono = input("Ingrese el teléfono: ")
+    empresa = input("Ingrese la empresa: ")
+    cargo = input("Ingrese el cargo: ")
+    return nombre, telefono, empresa, cargo
+#Funcion para capturar los datos del segundo menu
+def pedir_Datos_Contacto_Profesional():
+    nombre = input("Ingrese el nombre: ")
+    telefono = input("Ingrese el teléfono: ")
+    empresa = input("Ingrese la empresa: ")
+    cargo = input("Ingrese el cargo: ")
+    edad  = input("Ingrese su edad")
+    correo = input("Ingrese su correo")
+    whatsapp = input("Tiene  WhatsApp?: ")
+    facebook = input("Ingrese el perfil de Facebook: ")
+    instagram = input("Ingrese el perfil de Instagram: ")
+    return nombre, telefono, empresa, cargo,edad,correo,whatsapp, facebook, instagram
 
 # ACA PARTE SEBA
 #
